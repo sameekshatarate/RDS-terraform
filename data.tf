0e193753aa7ccf440ae9c84a9533fe1606a6bc72
sameekshatarate <165119806+sameekshatarate@users.noreply.github.com>
2024-05-14 15:17:16 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -4,8 +4,8 @@ data "aws_vpc" "vpc_available" {
     values = ["tcw_vpc"]
   }
 }
-data "aws_subnet_ids" "available_db_subnet" {
-  vpc_id = data.aws_vpc.vpc_available.id
+data "aws_subnets" "available_db_subnet" {
+  //vpc_id = data.aws_vpc.vpc_available.id
   filter {
     name   = "tag:Name"
     values = ["tcw_database*"]
